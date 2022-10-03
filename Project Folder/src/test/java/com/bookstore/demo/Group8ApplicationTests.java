8fece8c00bb837f0ae78dc453ee5855d6a0cba00
Cam Haley <89317514+camHaley@users.noreply.github.com>
2022-10-03 11:28:34 -0400

    Add files via upload

---
+++ Project Folder/src/test/java/com/bookstore/demo/Group8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
